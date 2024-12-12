e2c0d36a7ebce2d60ecafd03889a2b409f6e7c89
Subalakshmi K <Suba081996@gmail.com>
2024-12-12 23:08:37 +0530

    Postiveornegative.cpp

---
+++ Postiveornegative.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    
+    if ( num >=0)
+    {
+      cout << "Number is postive";
+    }
+    else
+    {
+      cout << "Number is negative";
+    }
+    return 0;
+}
